71abff1b3e71610cb85dc2d9ba8509d4897a8e37
xfy <i@rua.plus>
2026-06-04 15:49:30 +0800

    refactor: add style prop to SkeletonBox, migrate last tags_skeleton div

--- src/components/skeletons/atoms.rs
+++ src/components/skeletons/atoms.rs
@@ -1,10 +1,11 @@
 use dioxus::prelude::*;
 
 #[component]
-pub fn SkeletonBox(class: &'static str) -> Element {
+pub fn SkeletonBox(class: &'static str, style: Option<&'static str>) -> Element {
     rsx! {
         div {
             class: "bg-gray-200 dark:bg-[#2a2a2a] animate-pulse {class}",
+            style: style.unwrap_or(""),
         }
     }
 }
